36e92a2332fa6475ab2782c529ece79ef61ef225
Itzik Grossman <itzik@scrtlabs.com>
2023-12-29 18:19:23 +0200

    Added json-summary reporter for coverage CI

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     ],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'json-summary'],
     },
   },
 })

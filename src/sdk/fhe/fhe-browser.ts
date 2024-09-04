d42314477eca9efc8ee01f99ac2936c185349346
halftone-dev <halftonedev@gmail.com>
2024-09-04 16:38:48 +0100

    Remove logs

--- src/sdk/fhe/fhe-browser.ts
+++ src/sdk/fhe/fhe-browser.ts
@@ -1,3 +1,4 @@
+//@ts-expect-error imports from different wasm/js files are wonky
 import wasm from "./tfhe_bg.wasm";
 //@ts-expect-error imports from different wasm/js files are wonky
 import initSDK, { InitOutput } from "./tfhe.js";
@@ -7,7 +8,6 @@ export type InitFhevm = typeof initSDK;
 
 const initFhevm: InitFhevm = async () => {
   if (!initialized) {
-    console.log(wasm);
     try {
       initialized = await initSDK(wasm);
     } catch (_) {
@@ -19,9 +19,6 @@ const initFhevm: InitFhevm = async () => {
 
 export const asyncInitFhevm: () => Promise<void> = async () => {
   try {
-    // const { initFhevm } = await import("./init.js");
-    console.log("initFhevm");
-
     await initFhevm();
   } catch (err) {
     throw new Error(`Error initializing FhenixClient ${err}`);

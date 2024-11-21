50f85c44a8635efe1d6c42d92b2d09add0cbf8af
halftone-dev <halftonedev@gmail.com>
2024-11-21 15:51:53 +0000

    Remove sdk-v2

--- src/fhenix.ts
+++ src/fhenix.ts
@@ -5,7 +5,6 @@
 
 export { FhenixClient, FhenixClientSync } from "./sdk/index.js";
 
-export { fhenixsdk } from "./sdk/sdk-permitV2.js";
 export { PermitV2 } from "./sdk/permitV2/permitV2.js";
 
 export {
